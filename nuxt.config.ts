1bb3e88f24c807ddc2570ef0cff7d992bd38f093
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-12-01 10:39:03 +0100

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default defineNuxtConfig({
   build: {
     transpile: ["tslib"],
@@ -8,7 +11,21 @@ export default defineNuxtConfig({
   apollo: {
     clients: {
       default: {
-        httpEndpoint: "https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clpgr2zbkr4ju01t602sicyv3/master",
+        httpEndpoint:
+          "https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clpd3clevd29e01uie0ob3skk/master",
+      },
+    },
+  },
+  css: ["assets/font.css"],
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: ["Oswald", ...defaultTheme.fontFamily.sans],
+            serif: ["Libre Baskerville", ...defaultTheme.fontFamily.serif],
+          },
+        },
       },
     },
   },
