484a180f407590051f6cc89d4d33fc245b5b3d1b
aimielbn <aimie.lebon@ifosup.wavre.be>
2023-12-01 09:49:36 +0100

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  build: {
+    transpile: ["tslib"],
+  },
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/apollo", "@nuxtjs/tailwindcss", "@nuxt/image"],
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: "https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clpgr2zbkr4ju01t602sicyv3/master",
+      },
+    },
+  },
+});
